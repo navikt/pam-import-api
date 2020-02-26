f0f09b59b0e3cbd4fe4a7974465a615c1302b7ff
Tuan Tran <dinh.anh.tuan.tran@nav.no>
2020-02-26 20:45:04 +0100

    downgrading micronaut, shedlock is not compatible with latest version

--- src/main/kotlin/no/nav/arbeidsplassen/importapi/transferlog/TransferLogScheduler.kt
+++ src/main/kotlin/no/nav/arbeidsplassen/importapi/transferlog/TransferLogScheduler.kt
@@ -7,22 +7,22 @@ import net.javacrumbs.shedlock.micronaut.SchedulerLock
 import org.slf4j.LoggerFactory
 import javax.inject.Singleton
 
-@Around
 @Singleton
-@Requires(property = "transferlog.scheduler.enabled", value = "true")
+@Around
 class TransferLogScheduler(private val transferLogTasks: TransferLogTasks) {
 
     companion object {
         private val LOG = LoggerFactory.getLogger(TransferLogScheduler::class.java)
     }
-    @Scheduled(cron="0,10,20,30,40,50 * * * * *")
+
+    @Scheduled(cron="0 * * * * *")
     @SchedulerLock(name = "doTransferLogTask")
     fun startTransferLogTask() {
-        LOG.debug("starting transferlogtask")
+        LOG.info("starting transferlogtask")
         transferLogTasks.doTransferLogTask()
     }
 
-    @Scheduled(cron="05 15 00 * * * ")
+    @Scheduled(cron="05 15 00 * * *")
     @SchedulerLock(name="deleteTransferLogTask")
     fun startDeleteTransferLogTask() {
         LOG.info("starting deletTransferLogTask")
