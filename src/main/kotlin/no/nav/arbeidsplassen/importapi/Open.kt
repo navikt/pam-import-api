6a4510ba7fc82d03b9178e4e8fcf20d9003a7f77
Tuan Tran <dinh.anh.tuan.tran@nav.no>
2020-03-01 16:41:30 +0100

    updated micronaut to 1.3.2 and fix incompatibilities

---
+++ src/main/kotlin/no/nav/arbeidsplassen/importapi/Open.kt
@@ -0,0 +1,8 @@
+package no.nav.arbeidsplassen.importapi
+
+import java.lang.annotation.ElementType
+import java.lang.annotation.Target
+
+@Target(ElementType.ANNOTATION_TYPE)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class Open
--- src/main/kotlin/no/nav/arbeidsplassen/importapi/transferlog/TransferLogScheduler.kt
+++ src/main/kotlin/no/nav/arbeidsplassen/importapi/transferlog/TransferLogScheduler.kt
@@ -1,22 +1,22 @@
 package no.nav.arbeidsplassen.importapi.transferlog
 
-import io.micronaut.aop.Around
-import io.micronaut.context.annotation.Requires
+
 import io.micronaut.scheduling.annotation.Scheduled
 import net.javacrumbs.shedlock.micronaut.SchedulerLock
+import no.nav.arbeidsplassen.importapi.Open
 import org.slf4j.LoggerFactory
 import javax.inject.Singleton
 
 @Singleton
-@Around
+@Open
 class TransferLogScheduler(private val transferLogTasks: TransferLogTasks) {
 
     companion object {
         private val LOG = LoggerFactory.getLogger(TransferLogScheduler::class.java)
     }
 
-    @Scheduled(cron="*/10 * * * * *")
     @SchedulerLock(name = "doTransferLogTask")
+    @Scheduled(cron="*/10 * * * * *")
     fun startTransferLogTask() {
         LOG.info("starting transferlogtask")
         transferLogTasks.doTransferLogTask()
