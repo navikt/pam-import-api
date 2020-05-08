278e8e50900cf6e60fef17f1bc8e3c5afe2672c0
Tuan Tran <dinh.anh.tuan.tran@nav.no>
2020-05-08 09:09:49 +0200

    try again with escaping quotes

--- scripts/init-import-api.sh
+++ scripts/init-import-api.sh
@@ -6,4 +6,4 @@ export SERVICEUSER=$(cat /secrets/pamimportapi/serviceuser/credentials/username)
 export SERVICEUSER_PASSWORD=$(cat /secrets/pamimportapi/serviceuser/credentials/password)
 export KAFKA_SSL_TRUSTSTORE_LOCATION=${NAV_TRUSTSTORE_PATH}
 export KAFKA_SSL_TRUSTSTORE_PASSWORD=${NAV_TRUSTSTORE_PASSWORD}
-export KAFKA_SASL_JAAS_CONFIG="org.apache.kafka.common.security.plain.PlainLoginModule required username=${SERVICEUSER} password=${SERVICEUSER_PASSWORD}"
+export KAFKA_SASL_JAAS_CONFIG="org.apache.kafka.common.security.plain.PlainLoginModule required username=\"${SERVICEUSER}\" password=\"${SERVICEUSER_PASSWORD}\";"
