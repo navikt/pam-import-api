3d1aa9e1b4b1fd549b2a4ecd0b772f10450f31db
Tuan Tran <dinh.anh.tuan.tran@nav.no>
2020-05-08 09:50:21 +0200

    prod values

--- scripts/init-import-api.sh
+++ scripts/init-import-api.sh
@@ -7,3 +7,5 @@ export SERVICEUSER_PASSWORD=$(cat /secrets/pamimportapi/serviceuser/credentials/
 export KAFKA_SSL_TRUSTSTORE_LOCATION=${NAV_TRUSTSTORE_PATH}
 export KAFKA_SSL_TRUSTSTORE_PASSWORD=${NAV_TRUSTSTORE_PASSWORD}
 export KAFKA_SASL_JAAS_CONFIG="org.apache.kafka.common.security.plain.PlainLoginModule required username=\"${SERVICEUSER}\" password=\"${SERVICEUSER_PASSWORD}\";"
+export KAFKA_SASL_MECHANISM=PLAIN
+export KAFKA_SECURITY_PROTOCOL=SASL_SSL
