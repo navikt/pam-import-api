5745e0e4d65ca6a894cad162ae42bc3bb6586817
Tuan Tran <dinh.anh.tuan.tran@nav.no>
2020-05-28 11:57:30 +0200

    added openapi libs

--- src/main/kotlin/no/nav/arbeidsplassen/importapi/Application.kt
+++ src/main/kotlin/no/nav/arbeidsplassen/importapi/Application.kt
@@ -1,7 +1,16 @@
 package no.nav.arbeidsplassen.importapi
 
 import io.micronaut.runtime.Micronaut
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.info.Info
 
+@OpenAPIDefinition(
+        info = Info(
+                title = "Arbeidsplassen import api",
+                version = "1.0",
+                description = "Import api for stillinger"
+        )
+)
 object Application {
 
     @JvmStatic
