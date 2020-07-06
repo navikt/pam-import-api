524cf0bd89fab888d434ff703d1708dad3879677
Tuan Tran <dinh.anh.tuan.tran@nav.no>
2020-07-06 11:59:14 +0200

    fix jdbc url env

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,6 @@ FROM navikt/java:11
 
 RUN apt-get update && apt-get install -y curl
 
-COPY scripts/init-kafka-env.sh /init-scripts/init-kafka-env.sh
+COPY scripts/init-pam-import-env.sh /init-scripts/init-pam-import-env.sh
 COPY build/libs/pam-import-api-*-all.jar ./app.jar
 ENV JAVA_OPTS="-Xms256m -Xmx1536m -Doracle.jdbc.autoCommitSpecCompliant=false"
--- scripts/init-pam-import-env.sh
+++ scripts/init-pam-import-env.sh
@@ -3,3 +3,4 @@ export KAFKA_SSL_TRUSTSTORE_PASSWORD=${NAV_TRUSTSTORE_PASSWORD}
 export KAFKA_SASL_JAAS_CONFIG="org.apache.kafka.common.security.plain.PlainLoginModule required username=\"${SERVICEUSER}\" password=\"${SERVICEUSER_PASSWORD}\";"
 export KAFKA_SASL_MECHANISM=PLAIN
 export KAFKA_SECURITY_PROTOCOL=SASL_SSL
+export DB_JDBC_URL="jdbc:${DB_URL}"
