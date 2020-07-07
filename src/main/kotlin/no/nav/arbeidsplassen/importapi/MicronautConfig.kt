2d44b21f698ed352e76d3ee1e48cac9b1a1919ea
Tuan Tran <dinh.anh.tuan.tran@nav.no>
2020-07-07 11:24:09 +0200

    testing

--- src/main/kotlin/no/nav/arbeidsplassen/importapi/MicronautConfig.kt
+++ src/main/kotlin/no/nav/arbeidsplassen/importapi/MicronautConfig.kt
@@ -1,14 +1,16 @@
 package no.nav.arbeidsplassen.importapi
 
 import io.micronaut.context.annotation.Factory
+import io.micronaut.context.annotation.Replaces
 import net.javacrumbs.shedlock.core.LockProvider
 import net.javacrumbs.shedlock.provider.jdbc.JdbcLockProvider
 import no.nav.pam.yrkeskategorimapper.StyrkCodeConverter
+import javax.inject.Named
 import javax.inject.Singleton
 import javax.sql.DataSource
 
 @Factory
-class MicronautConfig(private val dataSource: DataSource) {
+class MicronautConfig( @Named("shedlock") private val dataSource: DataSource) {
 
     @Singleton
     fun lockProvider(): LockProvider {
