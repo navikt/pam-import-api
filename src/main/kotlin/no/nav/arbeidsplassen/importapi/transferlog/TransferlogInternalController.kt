49d66f980d3eaa7bf5daec7f817affaaa06ae8a6
Tuan Tran <dinh.anh.tuan.tran@nav.no>
2020-09-25 09:50:07 +0200

    internal_controllers

---
+++ src/main/kotlin/no/nav/arbeidsplassen/importapi/transferlog/TransferlogInternalController.kt
@@ -0,0 +1,25 @@
+package no.nav.arbeidsplassen.importapi.transferlog
+
+import io.micronaut.data.model.Pageable
+import io.micronaut.data.model.Slice
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Get
+import io.micronaut.http.annotation.QueryValue
+import io.swagger.v3.oas.annotations.Hidden
+import org.slf4j.LoggerFactory
+import java.time.LocalDateTime
+
+@Controller("/internal/transfers")
+@Hidden
+class TransferlogInternalController(private val transferLogRepository: TransferLogRepository) {
+
+    companion object {
+        private val LOG = LoggerFactory.getLogger(TransferlogInternalController::class.java)
+    }
+
+    @Get("/")
+    fun getAllTransferlogs(@QueryValue updated: String, pageable: Pageable): Slice<TransferLog> {
+        return transferLogRepository.findByUpdatedGreaterThanEquals(LocalDateTime.parse(updated),pageable)
+    }
+
+}
