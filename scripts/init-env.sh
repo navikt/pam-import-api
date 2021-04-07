37c2fd6a771ebcacc781073ff6f82fa8cd5cdb5e
Tuan Tran <dinh.anh.tuan.tran@nav.no>
2021-04-07 08:56:49 +0200

    switch to aiven kafka

--- scripts/init-env.sh
+++ scripts/init-env.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 # PUT all aiven KAFKA config into env variables, will be auto picked up by kafka
+if test -r "${KAFKA_TRUSTSTORE_PATH}";then
 export KAFKA_SSL_TRUSTSTORE_LOCATION=${KAFKA_TRUSTSTORE_PATH}
 export KAFKA_SSL_TRUSTSTORE_PASSWORD=${KAFKA_CREDSTORE_PASSWORD}
 export KAFKA_SSL_KEYSTORE_LOCATION=${KAFKA_KEYSTORE_PATH}
@@ -10,4 +11,5 @@ export KAFKA_SSL_KEYSTORE_TYPE=PKCS12
 export KAFKA_SECURITY_PROTOCOL=SSL
 export KAFKA_HEALTH_ENABLED=false
 export KAFKA_BOOTSTRAP_SERVERS=${KAFKA_BROKERS}
+fi
 export DB_JDBC_URL="jdbc:postgresql://${DB_HOST}:5432/importapi"
