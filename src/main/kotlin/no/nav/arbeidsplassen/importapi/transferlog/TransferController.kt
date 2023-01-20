2b709691fb5d142ad398aa8331ed61667173f377
noraness <nora.storebo.ness@nav.no>
2023-01-20 11:48:17 +0100

    Change to info-logging

--- src/main/kotlin/no/nav/arbeidsplassen/importapi/transferlog/TransferController.kt
+++ src/main/kotlin/no/nav/arbeidsplassen/importapi/transferlog/TransferController.kt
@@ -40,7 +40,7 @@ class TransferController(private val transferLogService: TransferLogService,
 
     @Post("/batch/{providerId}")
     fun postTransfer(@PathVariable providerId: Long, @Body ads: List<AdDTO>): HttpResponse<TransferLogDTO> {
-        LOG.debug("Streaming ${ads.size} for provider $providerId")
+        LOG.info("Streaming ${ads.size} for provider $providerId")
         if (ads.size > adsSize || ads.isEmpty()) {
             throw ImportApiError("ads should be between 1 to max $adsSize", ErrorType.INVALID_VALUE)
         }
