506541760b0a4b77c200b52e0f18aa2f94307818
Oscar Vladau-Husevold <oscarnvh@gmail.com>
2023-04-26 15:14:45 +0200

    Bumper avhengigheter i frontendmodulen

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Preview from './preview/Preview';
 import { StoreProvider } from './context/StoreContext';
 import './styles.less';
@@ -9,9 +9,9 @@ import './styles.less';
 const Index = () => {
     return (
         <main>
-            <Switch>
+            <Routes>
                 <Route exact path={`${__CONTEXT_PATH__}/:uuid`} component={Preview} />
-            </Switch>
+            </Routes>
         </main>
 
     );
