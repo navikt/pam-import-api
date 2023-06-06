08c8a162c0a04bb0b7c3f0978745b40d8213c258
noraness <nora.storebo.ness@nav.no>
2023-06-06 14:25:43 +0200

    Forward adpreview to arbeidsplassen

--- src/main/kotlin/no/nav/arbeidsplassen/importapi/adstate/AdPreviewController.kt
+++ src/main/kotlin/no/nav/arbeidsplassen/importapi/adstate/AdPreviewController.kt
@@ -1,6 +1,8 @@
 package no.nav.arbeidsplassen.importapi.adstate
 
+import io.micronaut.context.annotation.Value
 import io.micronaut.context.env.Environment
+import io.micronaut.http.HttpResponse
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.PathVariable
@@ -8,13 +10,15 @@ import io.micronaut.http.server.types.files.StreamedFile
 import io.swagger.v3.oas.annotations.Hidden
 import no.nav.arbeidsplassen.importapi.dto.AdDTO
 import org.slf4j.LoggerFactory
+import java.net.URI
+import java.net.URL
 import java.util.*
 import javax.annotation.security.PermitAll
 
 @PermitAll
 @Controller
 @Hidden
-class AdPreviewController(private val adStateService: AdStateService, private val environment: Environment) {
+class AdPreviewController(private val adStateService: AdStateService, private val environment: Environment, @Value("\${ad.preview.url}") private val previewUrl: String) {
 
     companion object {
         private val LOG = LoggerFactory.getLogger(AdPreviewController::class.java)
@@ -27,8 +31,8 @@ class AdPreviewController(private val adStateService: AdStateService, private va
     }
 
     @Get("/frontend/{uuid}")
-    fun forwardIndexHtml(uuid: String) : Optional<StreamedFile> {
-        LOG.info("Previewing ad. Uuid: {}", uuid)
-        return environment.getResource("classpath:frontend/index.html").map { StreamedFile(it) }
+    fun forwardIndexHtml(uuid: String) : HttpResponse<URI> {
+        LOG.info("Previewing ad. Redirecting to {} for uuid: {}", previewUrl, uuid)
+        return HttpResponse.redirect(URI("$previewUrl/$uuid"))
     }
 }
