c622efb7978638ea7973ba45e2bd2261d1ab24e4
noraness <nora.storebo.ness@nav.no>
2023-06-12 11:59:11 +0200

    Receive referrerfield

--- src/main/kotlin/no/nav/arbeidsplassen/importapi/dto/TransferDTO.kt
+++ src/main/kotlin/no/nav/arbeidsplassen/importapi/dto/TransferDTO.kt
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonEnumDefaultValue
 import no.nav.arbeidsplassen.importapi.properties.PropertyNames
 import java.time.LocalDateTime
 
-data class AdDTO(val reference: String, val published: LocalDateTime?, val expires: LocalDateTime?,
+data class AdDTO(val reference: String, val referrer: String? = "", val published: LocalDateTime?, val expires: LocalDateTime?,
                  val contactList: List<ContactDTO> = listOf(), val locationList: List<LocationDTO> = listOf(),
                  val properties: Map<PropertyNames, String> = hashMapOf(), val title: String, val adText: String,
                  val status:  AdStatus = AdStatus.RECEIVED, val privacy: PrivacyType = PrivacyType.SHOW_ALL,
