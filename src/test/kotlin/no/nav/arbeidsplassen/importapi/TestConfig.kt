1668eb67b44bc8457b6cc35a3e3e9151508d115e
noraness <nora.storebo.ness@nav.no>
2023-10-18 10:36:22 +0200

    TestConfig

---
+++ src/test/kotlin/no/nav/arbeidsplassen/importapi/TestConfig.kt
@@ -0,0 +1,28 @@
+package no.nav.arbeidsplassen.importapi
+
+import io.micronaut.context.annotation.Factory
+import io.micronaut.context.annotation.Replaces
+import jakarta.inject.Singleton
+import no.nav.arbeidsplassen.importapi.ontologi.KonseptGrupperingDTO
+import no.nav.arbeidsplassen.importapi.ontologi.LokalOntologiGateway
+import no.nav.arbeidsplassen.importapi.ontologi.Typeahead
+
+@Factory
+class TestConfig {
+
+
+    @Replaces(LokalOntologiGateway::class)
+    @Singleton
+    class MockLokalOntologiGateway : LokalOntologiGateway("URL") {
+        @Override
+        override fun hentTypeaheadStilling(stillingstittel : String) : List<Typeahead> {
+            return listOf()
+        }
+
+        @Override
+        override fun hentStyrkOgEscoKonsepterBasertPaJanzz(konseptId: String): KonseptGrupperingDTO? {
+            return null
+        }
+    }
+
+}
