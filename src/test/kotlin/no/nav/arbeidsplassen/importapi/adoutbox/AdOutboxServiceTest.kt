fc333e877a24f4fdd7d79f26dae912d4ade337be
Oscar Vladau-Husevold <oscarnvh@gmail.com>
2024-02-01 09:32:01 +0100

    Fikser brukne tester

--- src/test/kotlin/no/nav/arbeidsplassen/importapi/adoutbox/AdOutboxServiceTest.kt
+++ src/test/kotlin/no/nav/arbeidsplassen/importapi/adoutbox/AdOutboxServiceTest.kt
@@ -1,17 +1,40 @@
 package no.nav.arbeidsplassen.importapi.adoutbox
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
 import no.nav.arbeidsplassen.importapi.adstate.AdState
+import no.nav.arbeidsplassen.importapi.adstate.AdStateService
+import no.nav.arbeidsplassen.importapi.dto.AdDTO
+import no.nav.arbeidsplassen.importapi.dto.EmployerDTO
+import no.nav.arbeidsplassen.importapi.dto.LocationDTO
+import no.nav.arbeidsplassen.importapi.provider.ProviderDTO
+import no.nav.arbeidsplassen.importapi.provider.ProviderService
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import java.time.LocalDateTime
+import java.util.*
 
 @MicronautTest
 class AdOutboxServiceTest(
-    private val adOutboxService: AdOutboxService, private val adOutboxRepository: AdOutboxRepository
+    private val adOutboxService: AdOutboxService,
+    private val adOutboxRepository: AdOutboxRepository,
+    private val objectMapper: ObjectMapper,
+    private val providerService: ProviderService
 ) {
     @Test
-    fun `AdOutboxKafkaProducer starter og klarer å produsere melding`() {
-        adOutboxService.lagreTilOutbox(AdState(providerId = 1, reference = "ref", versionId = 1, jsonPayload = """{"payload": true}"""))
+    fun `AdOutboxService prosesserer og markerer outboxelementer riktig`() {
+        val provider = providerService.save(ProviderDTO(identifier = "test", email = "test", phone = "test"))
+
+        val ad = AdDTO(
+            published = LocalDateTime.now(), expires = LocalDateTime.now(),
+            adText = "adText", employer = EmployerDTO(null, "test", null, LocationDTO()),
+            reference = UUID.randomUUID().toString(), title = "title",
+            locationList = listOf(LocationDTO(country = "Danmark"))
+        )
+
+        adOutboxService.lagreTilOutbox(AdState(providerId = provider.id!!, reference = "ref", versionId = 1, jsonPayload = objectMapper.writeValueAsString(ad)))
 
         val initiellAdOutbox = adOutboxRepository.hentAlle().also { assertEquals(1, it.size) }.first()
         assertNull(initiellAdOutbox.prosessertDato)
